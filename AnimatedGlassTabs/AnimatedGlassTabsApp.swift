bb137aba7cc8fa50fb04fb1d049fc157bd5647b7
KaiChi <kai7win@gmail.com>
2025-10-21 13:55:45 +0800

    Initial Commit

---
+++ AnimatedGlassTabs/AnimatedGlassTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedGlassTabsApp.swift
+//  AnimatedGlassTabs
+//
+//  Created by Kai Chi Tsao on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedGlassTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
